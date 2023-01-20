554a524d1317ebe51f80c0312fbba1a941828cfc
ATZ-choosak <btk1392545@gmail.com>
2023-01-20 23:44:53 +0700

    realtime votting backend full version 1

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const { default: mongoose } = require('mongoose')
 const config = require('./config')
 const app = express()
 const TopicModel = require('./models/Topic')
+const cors = require('cors')
 
 mongoose.connect(config.apiPrefix).then(() => {
     console.log("Connected To Database.")
@@ -11,53 +12,85 @@ mongoose.connect(config.apiPrefix).then(() => {
 })
 
 app.use(express.json())
+app.use(cors())
+
+let clients = []
+
+const sendEventToAll = (data) => {
+    clients.forEach(client => client.res.write(`data: ${JSON.stringify(data)}\n\n`))
+}
 
 //Add Topic
-app.post('/add_topic' , async (req , res) => {
+app.post('/add_topic', async (req, res) => {
     const { topics } = await req.body
     topics.forEach(async element => {
-        const topic = await new TopicModel({title : element})
+        const topic = await new TopicModel({ title: element })
         await topic.save()
     });
-    res.status(200).send({message : `Now Added ${topics}`})
+    res.status(200).send({ message: `Now Added ${topics}` })
 
 })
 
 //User Vote
-app.post('/voted' , async (req , res) => {
-    const {user , id} = req.body
-    const topic_selection = await TopicModel.findOne({_id : id})
+app.post('/voted', async (req, res) => {
+    const { user, id } = req.body
+    const topic_selection = await TopicModel.findOne({ _id: id })
     let status = false
-    if(await topic_selection.votes.includes(user)){
+    if (await topic_selection.votes.includes(user)) {
         topic_selection.votes = await topic_selection.votes.filter(e => e !== user)
         status = false
-    }else{
+    } else {
         await topic_selection.votes.push(user)
         status = true
     }
 
     await topic_selection.save()
-    res.status(200).send({user , id , status : status})
+    sendEventToAll({ user, id, status: status })
+    res.status(200).send({ user, id, status: status })
 })
 
 //Get Topics
-app.get('/topics' , async (req , res) => {
+app.get('/topics', async (req, res) => {
     const { user } = req.body
     const topics = await TopicModel.find({})
     const topics_to_send = []
     for (let i = 0; i < topics.length; i++) {
         let userInVote = topics[i].votes.includes(user)
         topics_to_send.push({
-            _id : topics[i]._id,
-            title : topics[i].title,
-            votes : topics[i].votes.length,
-            status  :userInVote
+            _id: topics[i]._id,
+            title: topics[i].title,
+            votes: topics[i].votes.length,
+            status: userInVote
         })
 
     }
     res.status(200).send(topics_to_send)
 })
 
-app.listen(config.port , () => {
+app.get('/', (req, res) => {
+
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+    res.writeHead(200, headers);
+
+    const clientId = Date.now();
+
+    const newClient = {
+        id: clientId,
+        res
+    };
+
+    clients.push(newClient);
+
+    req.on('close', () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
+})
+
+app.listen(config.port, () => {
     console.log("Server is Running...")
 })
