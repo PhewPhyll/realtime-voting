ff86792fd0803de3e0bae7fb6ab2b236d9eca3d5
KixarS <teoylimhao2@gmail.com>
2023-01-21 13:28:39 +0700

    Appbar and Scrollbar

---
+++ front-end/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import barcampLogo from '../../image/barcampLogo.png';
+import './Navbar.css';
+
+function ResponsiveAppBar() {
+  return (
+    <AppBar position="static" style={{ background: "#000000"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Button href='/'>
+            <img style={{ width: "100%", height: 50}} src={barcampLogo} alt="BarcampLogo" href="/"  sx={{ display: { xs: 'none', md: 'flex' }}}/>
+          </Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
