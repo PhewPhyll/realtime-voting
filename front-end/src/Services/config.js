deb6ff99e945d098071b3a6e6b3b5f7c13042209
ATZ-choosak <btk1392545@gmail.com>
2023-01-25 01:25:02 +0700

    change config

--- front-end/src/Services/config.js
+++ front-end/src/Services/config.js
@@ -2,7 +2,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
     isProd,
-    apiUrlPrefix: isProd ? '' : 'http://192.168.1.7:8080',
+    apiUrlPrefix: isProd ? '' : 'http://localhost:8080',
     key : isProd ? process.env.KEY : 'barcamp007'
 }
 
