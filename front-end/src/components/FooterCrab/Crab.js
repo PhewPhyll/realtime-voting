0f1872974c2237c460fe06bf0ba4bdf8c4e23844
KixarS <teoylimhao2@gmail.com>
2023-01-25 15:50:03 +0700

    Create decorate Crab but not using yet

---
+++ front-end/src/components/FooterCrab/Crab.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Crab.css'
+
+function Crab() {
+  return (
+    <div class="crab-container">
+      <div class="crab-hand-l"></div>
+      <div class="crab-hand-r"></div>
+      <div class="crab-eyes"></div>
+      <div class="crab-leg-l leg-l-1"></div>
+      <div class="crab-leg-r leg-r-1"></div>
+      <div class="crab-leg-l leg-l-2"></div>
+      <div class="crab-leg-r leg-r-2"></div>
+      <div class="crab-leg-l leg-l-3"></div>
+      <div class="crab-leg-r leg-r-3"></div>
+      <div class="crab-body"></div>
+    </div>
+  )
+}
+
+export default Crab
