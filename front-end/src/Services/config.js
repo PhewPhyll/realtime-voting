5d90536bde855793ebd3822f500e9125080ee62a
Plub <plub.ptn@gmail.com>
2023-01-27 02:22:08 +0700

    fix key for aes

--- front-end/src/Services/config.js
+++ front-end/src/Services/config.js
@@ -3,7 +3,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 const config = {
     isProd,
     apiUrlPrefix: isProd ? '/api' : 'http://localhost:3001/api',
-    key : isProd ? process.env.KEY : 'barcamp007'
+    key : isProd ? process.env.REACT_APP_KEY : 'barcamp007'
 }
 
 export default config
