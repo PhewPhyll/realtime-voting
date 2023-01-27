0ce09d1f1167d6bc8aec188a8eebfef710f02e4f
ATZ-choosak <btk1392545@gmail.com>
2023-01-28 00:12:12 +0700

    fixed incommingNow warp text

--- show-score/src/Components/IncommingNow.js
+++ show-score/src/Components/IncommingNow.js
@@ -1,4 +1,4 @@
-import { Card, CardContent,  Typography } from '@mui/material'
+import { Card, CardContent, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
@@ -18,21 +18,28 @@ function IncommingNow({ title, callback }) {
 
     return (
         <AnimatePresence>
-            {show && 
+            {show &&
                 <motion.div
                     key={title}
                     layout
                     initial={{ scale: 0 }}
                     animate={{ scale: 1 }}
                     exit={{ translateY: 500, scale: 0, opacity: 0, transition: { duration: 0.5 } }}>
-                    <Card elevation={5} sx={{ position: 'relative' , height : '300px' }}>
+                    <Card elevation={5} sx={{ position: 'relative', height: '300px' }}>
                         <CardContent sx={{
-                            display : 'flex',
-                            alignItems : 'center',
-                            justifyContent : 'center',
-                            height : '90%'
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: '90%'
                         }}>
-                            <Typography noWrap variant='h4'>{title}</Typography>
+                            <Typography sx={{
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                display: "-webkit-box",
+                                whiteSpace: 'pre-wrap',
+                                WebkitLineClamp: 4,
+                                WebkitBoxOrient: "vertical"
+                            }} variant='h4'>{title}</Typography>
                         </CardContent>
                         <motion.div
                             initial={{ width: '100%' }}
@@ -42,12 +49,12 @@ function IncommingNow({ title, callback }) {
                                 backgroundColor: '#52cfe3',
                                 width: '100%',
                                 height: '0.4rem',
-                                position : 'absolute',
-                                bottom : 0
+                                position: 'absolute',
+                                bottom: 0
                             }} />
                     </Card>
                 </motion.div>
-    }
+            }
         </AnimatePresence>
     )
 }
