8e82c21bef3b4afc8e059435e18acb46fb43697e
psu6510110357 <phatsawut.phuket1@gmail.com>
2024-01-25 18:38:33 +0700

    fix background

--- show-score/src/Components/Vote/Votepage.js
+++ show-score/src/Components/Vote/Votepage.js
@@ -150,134 +150,135 @@ function Votepage() {
 
   return (
     <div style={{ background: 'linear-gradient(180deg, rgba(225,149,0,1) 0%, rgba(231,0,0,1) 100%)' }}>
-    <Container maxWidth="90%" sx={{ mt: '3rem' }}>
-      <Grid container columns={10} spacing={2} sx={{ height: '90vh', }}>
-        <Grid item xl={4}>
-          <Card elevation={5} sx={{ 
-            height: '100%',
-           borderRadius: '1rem', 
-           position: 'relative',
-           background : '#fff',
-           }}>
-            <CardContent sx={{ height: '100%' }}>
-              <Typography sx={{ textAlign: "center" }} fontWeight='bold' color='#FF0000' variant='h2'>TOP 10</Typography>
-              <Grid
-                sx={{ height: '100%', mt: '1rem' }}
-                container
-                columns={12}
-                spacing={2}
-                justifyContent='flex-start'
-                alignContent='start'
+      <Container maxWidth="90%" sx={{ pt: '3rem', pb: '4rem' }}>
+        <Grid container columns={10} spacing={2} sx={{ height: '90vh', }}>
+          <Grid item xl={4}>
+            <Card elevation={5} sx={{ 
+              height: '100%',
+            borderRadius: '1rem', 
+            position: 'relative',
+            background : '#fff',
+            }}>
+              <CardContent sx={{ height: '100%' }}>
+                <Typography sx={{ textAlign: "center" }} fontWeight='bold' color='#FF0000' variant='h2'>TOP 10</Typography>
+                <Grid
+                  sx={{ height: '100%', mt: '1rem' }}
+                  container
+                  columns={12}
+                  spacing={2}
+                  justifyContent='flex-start'
+                  alignContent='start'
+                >
+                  <AnimatePresence>
+                    {allTopics.sort((a, b) => b.votes - a.votes).filter(e => e.votes >= 3).slice(0, 10).map((e, i) =>
+                      <Grid item xl={6} key={e._id}>
+                        <motion.div
+                          key={e._id}
+                          layout
+                          initial={{ translateY: 500 }}
+                          animate={{ translateY: 0 }}
+                          exit={{ translateY: 1000, opacity: 0, scale: 0 }}
+                          transition={{ delay: 0.1 * i }}
+                        >
+                          <CardTop index={i} data={e} />
+                        </motion.div>
+                      </Grid>)}
+                  </AnimatePresence>
+                </Grid>
+              </CardContent>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 2 }}
               >
-                <AnimatePresence>
-                  {allTopics.sort((a, b) => b.votes - a.votes).filter(e => e.votes >= 3).slice(0, 10).map((e, i) =>
-                    <Grid item xl={6} key={e._id}>
-                      <motion.div
-                        key={e._id}
-                        layout
-                        initial={{ translateY: 500 }}
-                        animate={{ translateY: 0 }}
-                        exit={{ translateY: 1000, opacity: 0, scale: 0 }}
-                        transition={{ delay: 0.1 * i }}
-                      >
-                        <CardTop index={i} data={e} />
-                      </motion.div>
-                    </Grid>)}
-                </AnimatePresence>
-              </Grid>
-            </CardContent>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 2 }}
-            >
-              <Crab />
-              <Crab_small />
-              <Box sx={{
-                backgroundColor: "#fecd64",
-                width: '100%',
-                height: '120px',
-                position: 'absolute',
-                bottom : 0,
-                zIndex : -2
-              }}>
-                <div style={{
-                  height: "3.12em",
-                  width: "4.37em",
-                  backgroundColor: "#B39659",
-                  borderRadius: "50%",
+                <Crab />
+                <Crab_small />
+                <Box sx={{
+                  backgroundColor: "#fecd64",
+                  width: '100%',
+                  height: '120px',
                   position: 'absolute',
-                  bottom: "80px",
-                  left: "-20px",
-                  boxShadow: "2.5em 0.62em 0 -0.62em #716242",
+                  bottom : 0,
+                  zIndex : -2
                 }}>
                   <div style={{
-                    backgroundColor : '#fecd64',
-                    position : 'absolute',
-                    bottom : '-40px',
-                    width : '100px',
-                    height : '50px'
-                  }} />
-                </div>
-                <div style={{
-                  height: "5em",
-                  width: "8em",
-                  backgroundColor: "#B39659",
-                  borderRadius: "50%",
-                  position: 'absolute',
-                  bottom: "70px",
-                  right: "-20px",
-                  boxShadow: "2.5em 0.62em 0 -0.62em #716242",
-                  transform : 'scaleX(-1)'
-                }}>
+                    height: "3.12em",
+                    width: "4.37em",
+                    backgroundColor: "#B39659",
+                    borderRadius: "50%",
+                    position: 'absolute',
+                    bottom: "80px",
+                    left: "-20px",
+                    boxShadow: "2.5em 0.62em 0 -0.62em #716242",
+                  }}>
+                    <div style={{
+                      backgroundColor : '#fecd64',
+                      position : 'absolute',
+                      bottom : '-40px',
+                      width : '100px',
+                      height : '50px'
+                    }} />
+                  </div>
                   <div style={{
-                    backgroundColor : '#fecd64',
-                    position : 'absolute',
-                    bottom : '-30px',
-                    width : '200px',
-                    height : '50px'
-                  }}  />
-                </div>
-              </Box>
-            </motion.div>
-          </Card>
+                    height: "5em",
+                    width: "8em",
+                    backgroundColor: "#B39659",
+                    borderRadius: "50%",
+                    position: 'absolute',
+                    bottom: "70px",
+                    right: "-20px",
+                    boxShadow: "2.5em 0.62em 0 -0.62em #716242",
+                    transform : 'scaleX(-1)'
+                  }}>
+                    <div style={{
+                      backgroundColor : '#fecd64',
+                      position : 'absolute',
+                      bottom : '-30px',
+                      width : '200px',
+                      height : '50px'
+                    }}  />
+                  </div>
+                </Box>
+              </motion.div>
+            </Card>
+          </Grid>
+          <Grid item xl={6}>
+            <Card elevation={5} sx={{ height: '100%', borderRadius: '1rem' }}>
+              <CardContent>
+                <Typography sx={{ textAlign: "center" }} fontWeight='bold' color='#FF0000' variant='h4'>Topics</Typography>
+                <Grid sx={{ height: '100%', mt: '1rem' }}
+                  container
+                  columns={12}
+                  spacing={1}
+                  alignContent="center">
+                  <AnimatePresence>
+                    {allTopics.sort((a, b) => b.votes - a.votes).filter(e => e.votes >= 3).slice(10, allTopics.length).map((e, i) =>
+                      <Grid key={e._id} item xl={4} sx={{ zIndex : 10, position : 'relative' }}>
+                        <motion.div
+                          key={e._id}
+                          layout
+                          initial={{ translateY: 500 }}
+                          animate={{ translateY: 0 }}
+                          exit={{ scale: 0, opacity: 0 }}
+                        >
+                          <CardRange data={e} />
+                        </motion.div>
+                      </Grid>
+                    )}
+                  </AnimatePresence>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xl={6}>
-          <Card elevation={5} sx={{ height: '100%', borderRadius: '1rem' }}>
-            <CardContent>
-              <Typography sx={{ textAlign: "center" }} fontWeight='bold' color='#FF0000' variant='h4'>Topics</Typography>
-              <Grid sx={{ height: '100%', mt: '1rem' }}
-                container
-                columns={12}
-                spacing={1}
-                alignContent="center">
-                <AnimatePresence>
-                  {allTopics.sort((a, b) => b.votes - a.votes).filter(e => e.votes >= 3).slice(10, allTopics.length).map((e, i) =>
-                    <Grid key={e._id} item xl={4} sx={{ zIndex : 10, position : 'relative' }}>
-                      <motion.div
-                        key={e._id}
-                        layout
-                        initial={{ translateY: 500 }}
-                        animate={{ translateY: 0 }}
-                        exit={{ scale: 0, opacity: 0 }}
-                      >
-                        <CardRange data={e} />
-                      </motion.div>
-                    </Grid>
-                  )}
-                </AnimatePresence>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-      {/* <Wave /> */}
-      <Button sx={{
-        position: 'absolute',
-        right: 30,
-        bottom: 30
-      }} variant='text' onClick={Summary}>Summary</Button>
-    </Container>
+        {/* <Wave /> */}
+        <Button sx={{
+          position: 'absolute',
+          right: 30,
+          bottom: 5,
+          fontSize: '18px' 
+        }} variant='text' onClick={Summary}>Summary</Button>
+      </Container>
     </div>
   )
 }
