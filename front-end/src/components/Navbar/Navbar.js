5319447762419642c944c6b1dd93e4f0552feebd
Passakorn Limseng <6510110356@email.psu.ac.th>
2024-01-26 19:20:58 +0700

    navbar fix text

--- front-end/src/components/Navbar/Navbar.js
+++ front-end/src/components/Navbar/Navbar.js
@@ -19,11 +19,12 @@ export default function NavBar({used_point}) {
             Realtime Voting
           </Typography>
           
-          <div style={{position: 'absolute', right: '0'}}>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{marginRight: '10px'}} width="18" height="18" fill="red" className="bi bi-suit-heart-fill" viewBox="0 0 16 16">
+          <div style={{position: 'absolute', right: '0', display:'flex', flexDirection: 'row'}}>
+            <svg xmlns="http://www.w3.org/2000/svg" style={{marginRight: '10px'}} width="22" height="22" fill="red" className="bi bi-suit-heart-fill" viewBox="0 0 16 16">
               <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1"/>
             </svg>
-            {10-used_point}
+            <Typography sx={{fontSize: 16,fontFamily: 'Noto Sans Thai'}}>{10-used_point}</Typography>
+            
           </div>
           
         </Toolbar>
